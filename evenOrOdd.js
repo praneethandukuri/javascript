ae635b0549c5d2d119aa4a40b03f031cc441558c
cricket_lover <152866355+praneethandukuri@users.noreply.github.com>
2024-10-07 19:21:01 +0530

    Create evenOrOdd.js

---
+++ evenOrOdd.js
@@ -0,0 +1,14 @@
+/* find if the number is odd or even
+ print '0' if number is even
+ print '1' if numbet is odd
+*/
+
+const number = 10;
+const zero = 0;
+const one = 1;
+
+if(number % 2 === 0){
+    console.log(zero);
+}else{
+    console.log(one);
+}
